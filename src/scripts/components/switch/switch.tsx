0550a69e12370f2e74b75ec89313bb447e0dcfbd
Burton Smith <burton@breakstuff.io>
2020-08-27 09:08:17 -0400

    clean up code

--- src/scripts/components/switch/switch.tsx
+++ src/scripts/components/switch/switch.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop, Host, Element, ComponentInterface, Event, EventEmitter } from '@stencil/core';
+import { Component, h, Prop, Host, ComponentInterface, Event, EventEmitter } from '@stencil/core';
 
 @Component({
     tag: 'ks-switch',
@@ -6,7 +6,6 @@ import { Component, h, Prop, Host, Element, ComponentInterface, Event, EventEmit
 })
 export class Switch implements ComponentInterface {
     switchId = `switch_${switchIds++}`;
-    @Element() $el: HTMLElement;
 
     @Prop() label: string;
     @Prop() name: string;
@@ -29,18 +28,13 @@ export class Switch implements ComponentInterface {
     }
 
     render() {
-        let classes = {
-            'ks-switch': true,
-            'disabled': this.disable
-        };
-
         return (
-            <Host class={classes}>
-                <label htmlFor={this.switchId} class="switch-label">{this.label}</label>
-                <button id={this.switchId} class="switch-control" role="switch" aria-checked={`${this.checked}`} onClick={() => this.handleClick()} disabled={this.disable}>
+            <Host class="ks-switch">
+                <button id={this.switchId} class="control" role="switch" aria-checked={`${this.checked}`} onClick={() => this.handleClick()} disabled={this.disable}>
                     <span class="track"></span>
                     <span class="toggle"></span>
                 </button>
+                <label htmlFor={this.switchId} class="label">{this.label}</label>
             </Host>
         );
     }
